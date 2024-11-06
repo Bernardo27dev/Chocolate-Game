5536bd5145cc3db02cead62c54656eb1f4ac7a52
viniciusdevjesus <viniciusjesusfilho08@gmail.com>
2024-11-06 00:43:00 -0300

    feat: removed unused logs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require("express")
 require('dotenv').config();
-console.log(process.env.MONGO_URI);
 const mongoose = require("mongoose")
 const path = require("path")
 const app = express()
